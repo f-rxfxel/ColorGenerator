ce26d0f4f68614e05f210f09a956b79ca9e521d2
Francisco R. M. Porfírio <80773101+f-rxfxel@users.noreply.github.com>
2023-03-26 18:17:24 -0300

    Add files via upload

---
+++ script-hex.js
@@ -0,0 +1,38 @@
+// HEX
+const hex = document.getElementById('hex');
+
+// Verifica se o input está vazio
+const generateHEX = document.getElementById("generateHEX")
+generateHEX.onclick = () => {
+  if (!hex.value) {
+    alert("Preencha o campo para gerar uma cor HEX")
+  } else {
+    generateBgHEX(hex.value)
+  }
+}
+
+// Valida os caracteres válidos
+hex.addEventListener('input', function () {
+  let inputValue = hex.value;
+  const validCharacters = /^[0-9a-fA-F]+$/;
+
+  if (inputValue === '' || inputValue[0] !== '#') {
+    inputValue = '#' + inputValue.replace(/[^0-9a-fA-F]+/g, '').slice(0, 6);
+    hex.value = inputValue;
+  } else if (inputValue.length > 7) {
+    hex.value = inputValue.slice(0, 7);
+  } else if (!validCharacters.test(inputValue.slice(1))) {
+    hex.value = '#' + inputValue.slice(1).replace(/[^0-9a-fA-F]+/g, '');
+  }
+});
+
+// Limpa os inputs
+const clearHEX = document.getElementById("clearHEX")
+clearHEX.onclick = () => hex.value = ""
+
+// Define a cor do background
+const bgHEX = document.getElementById("background")
+function generateBgHEX(color) {
+  bgHEX.style.backgroundColor = color
+  hex.value = ""
+}
---
+++ script-rgb.js
@@ -0,0 +1,38 @@
+// RGB
+const r = document.getElementById("r");
+const g = document.getElementById("g");
+const b = document.getElementById("b");
+const rgb = [r, g, b]
+
+// Verifica se o input está vazio
+const generateRGB = document.getElementById("generateRGB")
+generateRGB.onclick = () => {
+  if (!r.value || !g.value || !b.value) {
+    alert("Preencha todos os campos para gerar uma cor RGB")
+  } else {
+    generateBgRGB(`rgb(${r.value}, ${g.value}, ${b.value})`)
+  }
+}
+
+// Valida os caracteres válidos
+rgb.forEach(input => {
+  input.addEventListener("input", () => {
+    const value = input.value;
+    if (isNaN(value)) {
+      input.value = value.slice(0, -1);
+    } else if (value < 0 || value > 255) {
+      input.value = Math.min(Math.max(parseInt(value), 0), 255);
+    }
+  })
+});
+
+// Limpa os inputs
+const clearRGB = document.getElementById("clearRGB")
+clearRGB.onclick = () => rgb.forEach(input => input.value = "");
+
+// Define a cor do background
+const bgRGB = document.getElementById("background")
+function generateBgRGB(color) {
+  bgRGB.style.backgroundColor = color
+  rgb.forEach(input => input.value = "");
+}
